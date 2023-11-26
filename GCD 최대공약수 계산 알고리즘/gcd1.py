9b49cc7b371077250e7967a85b5e7996cb3ad62f
ks2colorworld <ks@colorworld.kr>
2023-11-26 16:21:34 +0900

    #1 최대공약수(GCD) 계산 알고리즘 작성

---
+++ GCD 최대공약수 계산 알고리즘/gcd1.py
@@ -0,0 +1,16 @@
+def gcd(a,b):
+  print('a : ',a)
+  print('b : ',b)
+  whileIndex=0 # while문 반복 횟수 
+  
+  while a!=0 and b != 0:
+    if a>b: a= a-b
+    else: b=b-a
+    whileIndex += 1
+    
+  print('while count : ', whileIndex)
+  return a+b
+
+result = print('최대공약수 gcd : ', gcd(2,100))
+
+print(result)
