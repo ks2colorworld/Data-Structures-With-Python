c32f259a5882aa945ea2af9abfc27bcf3ce94388
ks2colorworld <ks@colorworld.kr>
2023-12-08 19:52:06 +0900

    update test code

--- stack calculator 스택 활용 계산기/calculator_test.py
+++ stack calculator 스택 활용 계산기/calculator_test.py
@@ -1,8 +1,11 @@
-from calculator import change_to_postfix
+from calculator import change_to_postfix,calculate_postfix,calulate_infix
 
 # str = '1+2*3' # 123*+
 # str = '(1*2)+3' # 12*3+
 # str = '(1+2)*3' # 12+3*
-str = '(1+2)+3*4/5+(6*7-8)' # 12+34*5/67*8-++ # 39.4
+str = '(1+2)*3+7' 
+# str = '(1+2)+3*4/5+(6*7-8)' # 12+34*5/67*8-++ # 39.4
+# str = '(1*2-1)+3' 
 # print(change_to_postfix(str, True))
-print('postfix : ', change_to_postfix(str, False))
+# print('postfix : ', change_to_postfix(str, False))
+print('result : ', calulate_infix(str, False))
