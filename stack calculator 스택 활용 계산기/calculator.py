e6b3754aabd7543f075f89ebc9d55e1bbfc8b9b6
ks2colorworld <ks@colorworld.kr>
2023-12-08 19:53:12 +0900

    write code in python

--- stack calculator 스택 활용 계산기/calculator.py
+++ stack calculator 스택 활용 계산기/calculator.py
@@ -140,20 +140,46 @@ def change_to_postfix(infix_string, show_steps=False):
 def calculate_postfix(stack_postfix, show_steps=False):
   print('postfix : ', stack_postfix)
   # 피연산자 스택 준비
+  stack_for_operand = Stack()
   # 계산완료 결과값
   cal_result = 0
   # postfix의 첫번째 문자열(토큰이라 부른다)부터 마지막 문자열까지 아래 작업(1)을 반복한다
+  token_index = 0
+  while token_index < len(stack_postfix):
+    current_token = stack_postfix[token_index]
     # 토큰이 피연산자이면
+    if current_token.isdigit():
       # 피연산자 스택에 push한다
-      # continue
+      stack_for_operand.push(current_token)
+      token_index += 1
+      continue
     
     # 토큰이 연산자이면
+    if current_token in ('+','-','*','/'):
       # (예외 추가) 피연산자가 2개이상 남아 있지 않으면 오류
+      if len(stack_for_operand) < 2:
+        raise ValueError(f"error : operands count < 2 ")
+        break
       # 피연산자 스택에서 두번 pop한 후 
       # 첫번째 pop은 오른쪽 / 두번째 pop은 왼쪽 
+      right_operand = float(stack_for_operand.pop())
+      left_operand = float(stack_for_operand.pop())
+      temp_result = 0
       # 두 피연산자를 연산자로 계산한다
+      if current_token == '+':
+        temp_result = left_operand + right_operand
+      elif current_token == '-':
+        temp_result = left_operand - right_operand
+      elif current_token == '*':
+        temp_result = left_operand * right_operand
+      elif current_token == '/':
+        temp_result = left_operand / right_operand
       # 계산한 결과값을 피연산자 스택에 push한다
-      # continue
+      stack_for_operand.push(temp_result)
+      token_index += 1
+      continue
+    
+  cal_result = stack_for_operand.pop()
   return cal_result
 
 # change_to_postfix() + calculate_postfix()
