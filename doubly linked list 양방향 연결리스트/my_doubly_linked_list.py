92eb032409e81908d82c0c99efe610a0f95d40e7
ks2colorworld <ks@colorworld.kr>
2023-12-14 17:07:32 +0900

    splice(), pushFront(), pushBack()

--- doubly linked list 양방향 연결리스트/my_doubly_linked_list.py
+++ doubly linked list 양방향 연결리스트/my_doubly_linked_list.py
@@ -36,3 +36,85 @@ class DoublyLinkedList:
     return f'{p}{tail}->(headNode)'
   def __len__(self):
     return self.size
+  
+  def splice(self,a:Node,b:Node,x:Node):
+    """a~b까지의 Node들을 잘라내서 x Node 다음에 붙여 넣습니다.
+    조건1: a <-> ... <-> b (a 다음에 b가 나와야 한다)
+    조건2: a와 b사이에 head node와 x가 없음
+    
+    Args:
+        a (Node): 시작 Node
+        b (Node): 마지막 Node
+        x (Node): 붙여 넣을 Node 위치
+
+    Raises:
+        TypeError: All parameters must be an instance of Node.
+    """
+    if not isinstance(a, Node) or \
+      not isinstance(b, Node) or \
+      not isinstance(x, Node):
+        raise TypeError("All parameters must be an instance of Node.")
+    
+    # 조건1: a <-> ... <-> b (a 다음에 b가 나와야 한다)
+    # 조건2: a와 b사이에 head node와 x가 없음
+    
+    # ''' 
+    # 조건 확인 로직 (강의 내용에는 없음)
+    nextNode = a.next
+    # if a == nextNode:
+    #   raise ValueError('조건 1 : a 노드 뒤에 b 노드가 위치해야 한다')
+    is_b_after_a = (a == nextNode) # nextNode.key != None
+    while is_b_after_a is not True and nextNode.key != None:
+      if nextNode == b: # 조건1 만족 : a <-> ... <-> b (a 다음에 b가 있음)
+        is_b_after_a = True
+        break
+      if nextNode == x:
+        is_b_after_a = False
+        raise ValueError("조건 2 : a,b 사이에 x 노드가 없어야 한다")
+        break
+      nextNode = nextNode.next
+      if nextNode.key == None: # 조건1,2 불만족 : a 다음에 b가 나타나기 전에 head node가 있음
+        is_b_after_a = False 
+        break
+    if is_b_after_a is not True:
+      raise ValueError("조건 2 : a,b 사이에 head 노드가 없어야 한다")
+    # '''
+    
+    
+    # 전 : ap(a.prev) <-> a <-> ... <-> b <-> bn(b.next)
+    # 전 : x <-> xn(x.next)
+    # 후 : ap <-> bn
+    # 후 : x <-> a <-> ... <-> b <-> xn
+    
+    ap = a.prev
+    bn = b.next
+    xn = x.next
+    
+    ap.next = bn
+    bn.prev = ap
+    x.next = a 
+    a.prev = x
+    b.next = xn
+    xn.prev = b
+    
+  def moveAfter(self,a:Node,x:Node):
+    self.splice(a,a,x)
+  def moveBefore(self,a:Node,x:Node):
+    self.splice(a,a,x.prev)
+    
+  def insertAfter(self,x:Node,a:Node):
+    self.moveAfter(a,x)
+    self.size += 1
+  def insertBefore(self,x:Node,a:Node):
+    self.moveBefore(a,x)
+    self.size += 1
+    
+  def pushFront(self,key):
+    self.insertAfter(self.head,Node(key))
+  def pushBack(self,key):
+    self.insertBefore(self.head,Node(key))
+  
+  def pushNodeFront(self,a:Node):
+    self.insertAfter(self.head,a)
+  def pushNodeBack(self,a:Node):
+    self.insertBefore(self.head,a)
--- doubly linked list 양방향 연결리스트/test_code.py
+++ doubly linked list 양방향 연결리스트/test_code.py
@@ -2,3 +2,54 @@ from my_doubly_linked_list import Node, DoublyLinkedList
 
 L = DoublyLinkedList()
 print(L)
+'''
+head:node[None], size:0
+(tailNode)<-DummyNode->(headNode)
+'''
+
+h = Node()
+a = Node(1)
+h.next = a
+h.prev = a
+a.next = h
+a.prev = h
+b = Node(2)
+# ''' h에 b가 있(없)는 조건
+b.next = h
+b.prev = a 
+a.next = b 
+h.prev = b
+# '''
+x = Node(3)
+''' x가 a 다음에 있(없)는 조건
+a.next = x
+x.prev = a 
+x.next = b 
+b.prev = x 
+# '''
+''' x가 다른 h2 리스트에 있는 조건 
+h2 = Node()
+h2.next = x
+h2.prev = x
+x.next = h2
+x.prev = h2
+# '''
+
+# L.splice(a,b,x)
+
+
+L.pushFront(3)
+# L.pushBack(-1)
+n_1 = Node(-1)
+# L.insertBefore(L.head,n_1)
+L.pushNodeBack(n_1)
+L.pushBack(9)
+
+n5 = Node(5)
+L.insertAfter(n_1,n5)
+
+print(L)
+'''
+head:node[None], size:4
+(tailNode)<-DummyNode<->node[3]<->node[-1]<->node[5]<->node[9]->(headNode)
+#'''
