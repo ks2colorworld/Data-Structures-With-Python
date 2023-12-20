6b7d2ed4733cc2d4ac458fd8ae3d8f3e886403ad
ks2colorworld <ks@colorworld.kr>
2023-12-20 14:44:28 +0900

    #10 modify heapify_down() and add comment about BigO info

--- heap 이진트리구조/my_heap.py
+++ heap 이진트리구조/my_heap.py
@@ -7,7 +7,7 @@ class Heap:
     pass
   def delete_max(self):
     pass
-  def make_heap(self,A:list)->list:
+  def make_heap(self,A:list)->list: # BigO(n*t) - BigO(n*h) - BigO(n*log n) - BigO(n)
     B = A
     n = len(B)
     # for (k = n-1; 0 <= k; k-- ) {/*pass*/} # js
@@ -15,7 +15,7 @@ class Heap:
       # B[k] -> heap 성질을 만족하는 곳으로 서로 자리바뀜 
       self.heapify_down(k,B)
     return B
-  def heapify_down(self,k:int,A:list):
+  def heapify_down(self,k:int,A:list): # BigO(h) - BigO(log n) 
     n = len(A)
     # A[k], n 
     while self.__is_not_leaf_node(k,n):
@@ -31,19 +31,21 @@ class Heap:
         M = A[m]
         A[m] = K
         A[k] = M
-      k+=1
-      if not(k < n):
+        k=m
+      # k+=1
+      else:
+      # if not(k < n):
         break
   def __is_leaf_node(self,index, total_nodes):
     return not self.__is_not_leaf_node(index,total_nodes)
   def __is_not_leaf_node(self,index, total_nodes):
     left_child = 2 * index + 1
     right_child = 2 * index + 2
-    # print('left child:', left_child, ', right child:', right_child)
+    # print('index:', index, ', left child:', left_child, ', right child:', right_child)
     return left_child < total_nodes or right_child < total_nodes
   def __index_max(self,K,L,R,k,l,r):
     m = k 
-    if K < L and (R is None or R < L):
+    if K < L and (R is None or R <= L):
       m = l 
     elif R is not None and K < R and L < R: 
       m = r 
