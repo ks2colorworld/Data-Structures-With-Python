fa8264464f778c2884b509693ab4151d5045131b
ks2colorworld <ks@colorworld.kr>
2023-12-22 17:08:28 +0900

    #11 Node generator(iterator) implement

--- binary search tree BST 이진탐색트리/my_BT.py
+++ binary search tree BST 이진탐색트리/my_BT.py
@@ -8,8 +8,26 @@ class Node:
     self.right = right
     if right is not None:
       self.right.parent = self
+    self.__preorderList=[]
+    self.__current=-1
   def __str__(self) -> str:
     return str(self.key)
+  def __iter__(self):
+    self.__preorderList = self.preorder()
+    if 0<len(self.__preorderList):
+      self.__current = 0 # self.__preorderList[0]
+    else:
+      self.__current = -1
+    return self
+  def __next__(self):
+    if 0<=self.__current: # -1
+      x = self.__current
+      self.__current = x + 1
+      if x == len(self.__preorderList):
+        raise StopIteration
+      return self.__preorderList[x]
+    else:
+      raise StopIteration
   def preorder(self)->list:
     A = []
     if self is not None:
