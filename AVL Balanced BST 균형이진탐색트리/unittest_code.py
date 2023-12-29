617f27c88d048f2e3baf38e76bc39e6d92d6390c
ks2colorworld <ks@colorworld.kr>
2023-12-29 17:16:39 +0900

    #12 start writing test code - rotateRight() rotateLeft()

---
+++ AVL Balanced BST 균형이진탐색트리/unittest_code.py
@@ -0,0 +1,14 @@
+import unittest
+
+# ''' rotateLeft, rotateRight 
+print('1')
+# '''
+
+class TestAVLRotateRightMethod(unittest.TestCase):
+    pass
+
+class TestAVLRotateLeftMethod(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
