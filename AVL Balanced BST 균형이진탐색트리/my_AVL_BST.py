cf1964e01645ac1f446c6d0e1f05bc340c62da55
ks2colorworld <ks@colorworld.kr>
2023-12-29 17:14:31 +0900

    #12 write natural language code - insert()

--- AVL Balanced BST 균형이진탐색트리/my_AVL_BST.py
+++ AVL Balanced BST 균형이진탐색트리/my_AVL_BST.py
@@ -55,3 +55,26 @@ class AVL(BST):
     #       / \   / \       >   / C \ / \     
     #      / A \ / B \      >        / A \        
     
+  def insert(self, key):
+    v = super().insert(key)
+    if v is None:
+      return None
+    # set each Node's height to leafNode 
+      # 삽입되는 노드의 부모노드들을 추적하여 각 노드들의 height를 +1씩 증가시킨다.
+      # Node 클래스에 height를 추가한다. 
+      
+    # find X,Y,Z 
+      # Z : left, right의 노드의 높이(height)차이가 2 이상 차이나는 첫번째 부모 노드
+      # Y : 삽입된 v 노드가 포함된 Z.left (혹은 Z.right) 노드 
+      # X : 삽입된 v 노드가 포함된 Y.left (혹은 Y.right) 노드 
+      
+    # W = rebalance X,Y,Z 
+      # W = rebalance 후 가장 상위 노드
+      # Y가 Z.left 이면서, X가 Y.left 일 때 >> rotateRight(Z) 1회
+      # Y가 Z.right 이면서, X가 Y.right 일 때 >> rotateLeft(Z) 1회
+      # Y가 Z.left 이면서, X가 Y.right 일 때 >> rotateLeft(Y) 1회 >> rotateRight(Z) 1회
+      # Y가 Z.right 이면서, X가 Y.right 일 때 >> rotateLeft(Z) 1회
+      # Y가 Z.right 이면서, X가 Y.left 일 때 >> rotateRight(Y) 1회 >> rotateLeft(Z) 1회
+    
+    # Z is root Node? self.root = W 
+      # 루트 노드 변경 
