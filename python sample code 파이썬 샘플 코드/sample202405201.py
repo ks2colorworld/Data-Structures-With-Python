cdccbd3169ca86adad2d8adc326801624bd9e363
ks2colorworld <ks@colorworld.kr>
2024-05-20 01:06:35 +0900

    샘플코드 추가 (함수 작성 예시 +gpt)

---
+++ python sample code 파이썬 샘플 코드/sample202405201.py
@@ -0,0 +1,22 @@
+# 코드 작성 예시
+
+def add(a: int, b: int) -> int:
+    """
+    두 정수를 더한 값을 반환합니다.
+
+    :param a: 첫 번째 정수
+    :param b: 두 번째 정수
+    :return: 두 정수의 합
+    """
+    pass  # 나중에 구현할 내용
+
+def subtract(a: int, b: int) -> int:
+    """
+    두 정수를 뺀 값을 반환합니다.
+
+    :param a: 첫 번째 정수
+    :param b: 두 번째 정수
+    :return: 두 정수의 차
+    """
+    pass  # 나중에 구현할 내용
+
